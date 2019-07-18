589a1d17147f1d7e2090e1a7594420d09c428941
EmaSuriano <emanuel.suriano@gmail.com>
2019-07-18 18:27:40 +0200

    ignore ts for the moment

--- theme/src/cms/index.ts
+++ theme/src/cms/index.ts
@@ -5,9 +5,11 @@ import FileSystemBackend from 'netlify-cms-backend-fs';
 if (process.env.NODE_ENV === 'development') {
   config.backend = {
     name: 'file-system',
+    // @ts-ignore
     api_root: 'http://localhost:8000/api',
   };
 
+  // @ts-ignore
   config.display_url = 'http://localhost:8000';
   CMS.registerBackend('file-system', FileSystemBackend);
 }
